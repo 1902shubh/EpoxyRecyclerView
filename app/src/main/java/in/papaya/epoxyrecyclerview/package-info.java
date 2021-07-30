8774ae2e926db85dcbdadc216a890d706f9d4a44
Shubham Pandey <shubhampandey979271@gmail.com>
2021-07-30 16:51:06 +0530

    Initial commit

---
+++ app/src/main/java/in/papaya/epoxyrecyclerview/package-info.java
@@ -0,0 +1,9 @@
+package in.papaya.epoxyrecyclerview;
+
+import com.airbnb.epoxy.EpoxyDataBindingPattern;
+
+import in.papaya.epoxyrecyclerview.R;
+
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "item")
+interface Config {
+}
